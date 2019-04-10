27c4b7a926767d5894b742f798d18bcca97cae45
Sergey Smirnov <smirnov.sege@gmail.com>
2019-04-10 10:47:09 +0300

    osa2. puhelinluettelo step 8

---
+++ osa2/puhelinluettelo/src/components/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import personService from '../services/personService'
+
+import Filter from './Filter'
+import PersonForm from './PersonForm'
+import Person from './Person'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+
+    if (persons.some(person => person.name === newName))
+      return alert(`"${newName}" on jo taulukossa!`)
+
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+    personService
+      .create(newPerson)
+      .then(createdNote =>
+        setPersons(persons.concat(createdNote))
+      )
+  }
+
+  const names = persons.filter(({ name }) => {
+    return filter
+      ? name.toLowerCase().includes(filter.toLowerCase())
+      : true
+  }).map(({ id, name, number }) =>
+    <Person
+      key={id}
+      name={name}
+      number={number} />
+  )
+
+  return (
+    <>
+      <h1>Contacts list</h1>
+      <Filter 
+        handleChange={(e) => setFilter(e.target.value)} />
+      <h2>add new one</h2>
+      <PersonForm
+        handleNameChange={(e) => setNewName(e.target.value)}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+        handleSubmit={handleFormSubmit} />
+      <h2>contacts</h2>
+      {names}
+    </>
+  )
+}
+
+export default App
